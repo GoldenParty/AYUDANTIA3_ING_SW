0974f50beb74ad3cc2dff13d4b5b4a8ebd2abff6
Golden <jorge.tuschner2301@alumnos.ubiobio.cl>
2026-09-05 00:14:43 -0400

    feat: correccion brand schema

--- src/schemas/brand.schema.js
+++ src/schemas/brand.schema.js
@@ -1,17 +1,19 @@
 import { z } from 'zod';
 
-// Esquema para crear una categoría (POST)
-export const createCategorySchema = z.object({
+// Esquema para crear una marca (POST)
+export const createBrandSchema = z.object({
   name: z
     .string({ required_error: 'El nombre de la marca es obligatorio' })
-    .min(3, 'El nombre debe tener al menos 2 caracteres')
+    .min(3, 'El nombre debe tener al menos 3 caracteres')
     .max(80, 'El nombre no puede exceder los 80 caracteres'),
+
   country: z
     .string()
     .max(40, 'El país no puede superar los 40 caracteres')
     .optional(),
-    website: z
+
+  website: z
     .string()
+    .url()
     .optional()
-    .url(),
 });
