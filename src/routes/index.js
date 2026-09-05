b55c85425613b7465b7960434e7c8df7a76143aa
Golden <jorge.tuschner2301@alumnos.ubiobio.cl>
2026-09-05 00:11:23 -0400

    feat: correccion index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+
 import categoryRoutes from './category.routes.js';
 import productRoutes from './product.routes.js';
+import brandRoutes from './brand.routes.js';
+import reviewRoutes from './review.routes.js';
+
+
 
 const apiRouter = Router();
 
@@ -9,12 +14,14 @@ apiRouter.get('/health', (req, res) => {
   res.status(200).json({
     status: 'OK',
     timestamp: new Date().toISOString(),
-    service: 'TechStore API (Productos y Categorías)'
+    service: 'TechStore API'
   });
 });
 
 // Enrutadores modulares
 apiRouter.use('/categories', categoryRoutes);
 apiRouter.use('/products', productRoutes);
+apiRouter.use('/brands', brandRoutes);
+
 
 export default apiRouter;
